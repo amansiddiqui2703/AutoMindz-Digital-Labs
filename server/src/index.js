63cfceb1b6ff09f701f01b14f08d242848d460c5
Aman <amans@automindz.com>
2026-06-10 02:27:43 +0530

    Fix Sentry init crash due to outdated integrations syntax

--- server/src/index.js
+++ server/src/index.js
@@ -62,14 +62,6 @@ const app = express();
 
 Sentry.init({
     dsn: env.SENTRY_DSN || "",
-    integrations: [
-        // enable HTTP calls tracing
-        new Sentry.Integrations.Http({ tracing: true }),
-        // enable Express.js middleware tracing
-        new Sentry.Integrations.Express({ app }),
-        // Automatically instrument Node.js libraries and frameworks
-        ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
-    ],
     // Performance Monitoring
     tracesSampleRate: env.NODE_ENV === 'production' ? 0.1 : 1.0, //  Capture 100% of the transactions
     // Set sampling rate for profiling - this is relative to tracesSampleRate
